25885e52d39078b13d6722e93a7b4405d53808a4
jglgap <a23ricardosr@iessanclemente.net>
2025-10-30 11:02:41 +0100

    continuacion ejercicio bolechas 30/10

--- AD/UD02/bolechas/src/main/java/bolechas/application/GestorTablas.java
+++ AD/UD02/bolechas/src/main/java/bolechas/application/GestorTablas.java
@@ -1,7 +1,9 @@
 package bolechas.application;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
+import java.util.Scanner;
 
 import bolechas.connection.MySqlConnection;
 
@@ -62,4 +64,85 @@ public class GestorTablas {
         crearTablaCliente();
         crearTablaPedido();
     }
+
+    // Gestionar cliente
+    public void insertarCliente(String DNI, String nombre) {
+        try (Connection c = conexionBd()) {
+            String crearCliente = "INSERT INTO CLIENTE(dni,nombre) VALUES(?,?)";
+            try (PreparedStatement pst = c.prepareStatement(crearCliente)) {
+                pst.setString(1, DNI.toLowerCase());
+                pst.setString(2, nombre);
+                pst.executeUpdate();
+                System.out.println("Cliente creado con exito");
+            } catch (Exception e) {
+                System.out.println("Problemas con la consulta");
+            }
+
+        } catch (Exception e) {
+            System.out.println("ERROR inesperado");
+        }
+
+    }
+
+    public void actualizarCliente(String DNI, String newNombre) {
+        try (Connection c = conexionBd()) {
+            String actualizarCliente = "UPDATE CLIENTE SET nombre = ? WHERE dni = ?";
+            try (PreparedStatement pst = c.prepareStatement(actualizarCliente)) {
+                pst.setString(1, newNombre);
+                pst.setString(2, DNI);
+                pst.executeUpdate();
+                System.out.println("Actualizacion realizada correctamente");
+            } catch (Exception e) {
+                System.out.println("Error en la consulta");
+            }
+        } catch (Exception e) {
+            System.out.println("ERROR inesperado");
+        }
+
+    }
+
+    public void borrarCliente(String DNI) {
+        try (Connection c = conexionBd()) {
+            String borrarCliente = "DELETE FROM CLIENTE WHERE dni = ?";
+            try (PreparedStatement pst = c.prepareStatement(borrarCliente)) {
+                pst.setString(1, DNI);
+                pst.executeUpdate();
+                System.out.println("Borrado ejecutado con exito");
+            } catch (Exception e) {
+                System.out.println("Error en la consulta");
+            }
+        } catch (Exception e) {
+            System.out.println("ERROR INESPERADO");
+        }
+    }
+
+    public void gestionClientes(int option) {
+        Scanner sc = new Scanner(System.in); 
+            switch (option) {
+                case 1:
+                    System.out.println("Insertar DNI");
+                    String DNI = sc.nextLine();
+                    System.out.println("Insertar nombre");
+                    String nombre = sc.nextLine();
+                    insertarCliente(DNI, nombre);
+                    break;
+                case 2:
+                    System.out.println("Insertar del cliente a actualizar");
+                    String dniActualizar = sc.nextLine().toLowerCase();
+                    System.out.println("Insertar nuevo nombre");
+                    String nombreActualizar = sc.nextLine();
+                    actualizarCliente(dniActualizar, nombreActualizar);
+                    break;
+                case 3:
+                    System.out.println("Insertar dni del cliente a borrar");
+                    String dniBorrar = sc.nextLine().toLowerCase();
+                    borrarCliente(dniBorrar);
+                    break;
+                default:
+                    break;
+            }
+      
+
+    }
+
 }
--- AD/UD02/bolechas/src/main/java/bolechas/application/Launch.java
+++ AD/UD02/bolechas/src/main/java/bolechas/application/Launch.java
@@ -4,31 +4,33 @@ import java.util.Scanner;
 
 public class Launch {
     public static void main(String[] args) {
-    
-    Scanner sc = new Scanner(System.in);
-    int iterator = 0;
-    while (iterator != 9) {
-        System.out.println("1-a \n2-b \n3-c \n4-d");
-        int option = Integer.parseInt(sc.nextLine());
-    switch (option) {
-        case 1:
-            GestorBaseDatos gb = new GestorBaseDatos();
-            gb.CrearBaseDatos();
-            break;
-        case 2:
-            GestorTablas gt = new GestorTablas();
-            gt.inicializarTablas();;
-            break;
-        case 3:
-            System.out.println("C");
-            break;
-        default:
-            break;
-    }
+        GestorBaseDatos gb = new GestorBaseDatos();
+        GestorTablas gt = new GestorTablas();
+        Scanner sc = new Scanner(System.in);
+        int iterator = 0;
+        while (iterator != 9) {
+            System.out.println("1-a \n2-b \n3-c \n4-d");
+            int option = Integer.parseInt(sc.nextLine());
+            switch (option) {
+                case 1:
 
+                    gb.CrearBaseDatos();
+                    break;
+                case 2:
 
-    }
+                    gt.inicializarTablas();
+                    
+                    break;
+                case 3:
+                    System.out.println("1-Crear cliente \n" + "2-Actualizar cliente\n" + "3-Eliminar cliente");
+                    int opt = Integer.parseInt(sc.nextLine());
+                    gt.gestionClientes(opt);
+                    break;
+                default:
+                    break;
+            }
 
+        }
 
     }
 }
